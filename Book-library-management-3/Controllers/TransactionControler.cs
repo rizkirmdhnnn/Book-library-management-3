24d797bfa8aff60d58ea3b6ef9afcc75086b4702
rizpedia <achmadrizkiramadhan0101@gmail.com>
2023-11-28 18:30:59 +0700

    feat(TransactionControler) : add borrowingBook controler

--- Book-library-management-3/Controllers/TransactionControler.cs
+++ Book-library-management-3/Controllers/TransactionControler.cs
@@ -3,12 +3,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Book_library_management_3.Models.Repository;
+using Book_library_management_3.Models.Entity;
+using Book_library_management_3.Models.Context;
+using System.Windows.Forms;
+using static Guna.UI2.Native.WinApi;
 
 namespace Book_library_management_3.Controllers
 {
 
     public class TransactionControler
     {
+        private transactionsRepository _repository;
 
+        public int borrowingBook(Transactions transactions)
+        {
+            int result = 0;
+
+            if (transactions.transactions_id == default(int))
+            {
+                MessageBox.Show("TransactionID cannot be empty", "Information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 0;
+            }
+
+            if (string.IsNullOrEmpty(transactions.isbn))
+            {
+                MessageBox.Show("ISBN cannot be empty", "Information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 0;
+            }
+
+            if (string.IsNullOrEmpty(transactions.username))
+            {
+                MessageBox.Show("Username cannot be empty", "Information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 0;
+            }
+
+            if (string.IsNullOrEmpty(transactions.status))
+            {
+                MessageBox.Show("Status cannot be empty", "Information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 0;
+            }
+
+            if (string.IsNullOrEmpty(transactions.date))
+            {
+                MessageBox.Show("Date cannot be empty", "Information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 0;
+            }
+
+            using(DbContext context = new DbContext())
+            {
+                _repository = new transactionsRepository(context);
+                result = _repository.borrowingBook(transactions);
+            }
+
+            return result;
+        }
     }
 }
