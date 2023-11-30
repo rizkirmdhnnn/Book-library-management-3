b69137a984ff8a0377a9380e164007c3b8209320
rizpedia <achmadrizkiramadhan0101@gmail.com>
2023-11-30 11:08:54 +0700

    add(Contoler) : add controler History

---
+++ Book-library-management-3/Controllers/HistoryControler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Book_library_management_3.Models.Repository;
+using Book_library_management_3.Models.Entity;
+using Book_library_management_3.Models.Context;
+
+namespace Book_library_management_3.Controllers
+{
+    public class HistoryControler
+    {
+        private HistoryRepository _repository;
+        
+        public List<History> getAllHistory()
+        {
+            List<History> list = new List<History>();
+
+            using(DbContext context = new DbContext())
+            {
+                _repository = new HistoryRepository(context);
+                
+                list = _repository.getAllHistory();
+            }
+
+            return list;
+        }
+    }
+}
