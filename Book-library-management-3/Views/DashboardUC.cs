40e066e953abcadb1d5cd217323425f677df6738
rizpedia <achmadrizkiramadhan0101@gmail.com>
2023-12-02 13:27:56 +0700

    feat() : makes the recent display work

--- Book-library-management-3/Views/DashboardUC.cs
+++ Book-library-management-3/Views/DashboardUC.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Book_library_management_3.Controllers;
+using Book_library_management_3.Models.Entity;
 
 namespace Book_library_management_3.Views
 {
@@ -16,10 +17,13 @@ namespace Book_library_management_3.Views
         private TransactionControler transactionControler;
         private BooksControler booksControler;
         private UsersControler usersControler;
-        
+        private List<Users> listRecentMembers = new List<Users>();
+        private List<Books> listRecentBooks = new List<Books>();
+
         public DashboardUC()
         {
             InitializeComponent();
+            InisialisasiListView();
             loadData();
         }
 
@@ -33,9 +37,50 @@ namespace Book_library_management_3.Views
             txt_totalBook.Text = booksControler.getTotalBook().ToString();
             txt_borrowing.Text = transactionControler.getTotalBorrowingBook().ToString();
             txt_returned.Text = transactionControler.getTotalReturnedBook().ToString();
+
+            lv_RecentMembers.Items.Clear();
+            listRecentMembers = usersControler.getRecentMembers();
+
+
+            lv_RecentBooks.Items.Clear();
+            listRecentBooks = booksControler.getRecentBooks();
+
+
+            foreach (Users user in listRecentMembers)
+            {
+                var item = new ListViewItem();
+                item.SubItems.Add(user.username);
+                item.SubItems.Add(user.status);
+                lv_RecentMembers.Items.Add(item);
+            }
+
+
+            foreach (Books books in listRecentBooks)
+            {
+                var item = new ListViewItem();
+                item.SubItems.Add(books.title);
+                item.SubItems.Add(books.stocks.ToString());
+                lv_RecentBooks.Items.Add(item);
+            }
+
         }
 
+        private void InisialisasiListView()
+        {
+            lv_RecentBooks.View = System.Windows.Forms.View.Details;
+            lv_RecentBooks.FullRowSelect = true;
+            lv_RecentBooks.GridLines = true;
+            lv_RecentBooks.Columns.Add("", 0, HorizontalAlignment.Right);
+            lv_RecentBooks.Columns.Add("Book Title", 170, HorizontalAlignment.Left);
+            lv_RecentBooks.Columns.Add("Book Stocks", 170, HorizontalAlignment.Left);
 
+            lv_RecentMembers.View = System.Windows.Forms.View.Details;
+            lv_RecentMembers.FullRowSelect = true;
+            lv_RecentMembers.GridLines = true;
+            lv_RecentMembers.Columns.Add("", 0, HorizontalAlignment.Right);
+            lv_RecentMembers.Columns.Add("Username", 170, HorizontalAlignment.Left);
+            lv_RecentMembers.Columns.Add("Type Account", 170, HorizontalAlignment.Left);
 
-    }
+        } 
+     }
 }
