ee3d4687eb4116adc7626e63a22bcae98733cfda
rizpedia <achmadrizkiramadhan0101@gmail.com>
2023-12-03 10:16:33 +0700

    style : Added books view

--- Book-library-management-3/Views/BooksUC.Designer.cs
+++ Book-library-management-3/Views/BooksUC.Designer.cs
@@ -28,32 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lv_History = new System.Windows.Forms.ListView();
+            this.lv_Books = new System.Windows.Forms.ListView();
             this.guna2Panel9 = new Guna.UI2.WinForms.Guna2Panel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.txt_total = new System.Windows.Forms.Label();
             this.btn_add = new Guna.UI2.WinForms.Guna2Button();
             this.label2 = new System.Windows.Forms.Label();
             this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
             this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2Panel9.SuspendLayout();
+            this.guna2Panel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lv_History
+            // lv_Books
             // 
-            this.lv_History.HideSelection = false;
-            this.lv_History.Location = new System.Drawing.Point(28, 81);
-            this.lv_History.Name = "lv_History";
-            this.lv_History.Size = new System.Drawing.Size(817, 445);
-            this.lv_History.TabIndex = 17;
-            this.lv_History.UseCompatibleStateImageBehavior = false;
+            this.lv_Books.HideSelection = false;
+            this.lv_Books.Location = new System.Drawing.Point(28, 81);
+            this.lv_Books.Name = "lv_Books";
+            this.lv_Books.Size = new System.Drawing.Size(817, 445);
+            this.lv_Books.TabIndex = 17;
+            this.lv_Books.UseCompatibleStateImageBehavior = false;
             // 
             // guna2Panel9
             // 
-            this.guna2Panel9.Controls.Add(this.label1);
-            this.guna2Panel9.Controls.Add(this.btn_add);
+            this.guna2Panel9.Controls.Add(this.guna2Panel1);
+            this.guna2Panel9.Controls.Add(this.txt_total);
             this.guna2Panel9.Controls.Add(this.label2);
-            this.guna2Panel9.Controls.Add(this.guna2TextBox1);
             this.guna2Panel9.Dock = System.Windows.Forms.DockStyle.Top;
             this.guna2Panel9.FillColor = System.Drawing.Color.White;
             this.guna2Panel9.Location = new System.Drawing.Point(0, 0);
@@ -63,17 +64,17 @@
             this.guna2Panel9.Size = new System.Drawing.Size(860, 63);
             this.guna2Panel9.TabIndex = 14;
             // 
-            // label1
+            // txt_total
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Montserrat", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(21, 7);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 44);
-            this.label1.TabIndex = 11;
-            this.label1.Text = "10";
+            this.txt_total.AutoSize = true;
+            this.txt_total.BackColor = System.Drawing.Color.Transparent;
+            this.txt_total.Font = new System.Drawing.Font("Montserrat", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_total.Location = new System.Drawing.Point(21, 7);
+            this.txt_total.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txt_total.Name = "txt_total";
+            this.txt_total.Size = new System.Drawing.Size(54, 44);
+            this.txt_total.TabIndex = 11;
+            this.txt_total.Text = "10";
             // 
             // btn_add
             // 
@@ -88,8 +89,8 @@
             this.btn_add.ForeColor = System.Drawing.Color.Black;
             this.btn_add.Image = global::Book_library_management_3.Properties.Resources.Add_Option;
             this.btn_add.ImageAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.btn_add.Location = new System.Drawing.Point(764, 16);
-            this.btn_add.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn_add.Location = new System.Drawing.Point(273, 17);
+            this.btn_add.Margin = new System.Windows.Forms.Padding(2);
             this.btn_add.Name = "btn_add";
             this.btn_add.Size = new System.Drawing.Size(81, 31);
             this.btn_add.TabIndex = 10;
@@ -107,7 +108,6 @@
             this.label2.Size = new System.Drawing.Size(95, 21);
             this.label2.TabIndex = 9;
             this.label2.Text = "Total Books";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // guna2TextBox1
             // 
@@ -122,7 +122,7 @@
             this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox1.Location = new System.Drawing.Point(546, 16);
+            this.guna2TextBox1.Location = new System.Drawing.Point(43, 17);
             this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.guna2TextBox1.Name = "guna2TextBox1";
             this.guna2TextBox1.PasswordChar = '\0';
@@ -173,30 +173,43 @@
             this.guna2Button1.Text = "Edit Data";
             this.guna2Button1.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
             // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel1.Controls.Add(this.btn_add);
+            this.guna2Panel1.Controls.Add(this.guna2TextBox1);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.guna2Panel1.Location = new System.Drawing.Point(491, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(369, 63);
+            this.guna2Panel1.TabIndex = 12;
+            // 
             // BooksUC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.guna2Button2);
-            this.Controls.Add(this.lv_History);
+            this.Controls.Add(this.lv_Books);
             this.Controls.Add(this.guna2Button1);
             this.Controls.Add(this.guna2Panel9);
             this.Name = "BooksUC";
             this.Size = new System.Drawing.Size(860, 582);
             this.guna2Panel9.ResumeLayout(false);
             this.guna2Panel9.PerformLayout();
+            this.guna2Panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-        private System.Windows.Forms.ListView lv_History;
+        private System.Windows.Forms.ListView lv_Books;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel9;
         private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
         private Guna.UI2.WinForms.Guna2Button btn_add;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txt_total;
         private Guna.UI2.WinForms.Guna2Button guna2Button2;
         private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
     }
 }
--- Book-library-management-3/Views/BooksUC.cs
+++ Book-library-management-3/Views/BooksUC.cs
@@ -7,24 +7,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Book_library_management_3.Models.Entity;
+using Book_library_management_3.Controllers;
 
 namespace Book_library_management_3.Views
 {
     public partial class BooksUC : UserControl
     {
+        private BooksControler _booksControler;
+        List<History> histories = new List<History>();
+
+
         public BooksUC()
         {
+            _booksControler = new BooksControler();
             InitializeComponent();
+            InitializeListView();
+            loadData();
         }
 
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        private void loadData()
         {
+            txt_total.Text = _booksControler.getTotalBook().ToString();
 
         }
 
-        private void label2_Click(object sender, EventArgs e)
+        private void InitializeListView()
         {
-
+            lv_Books.View = System.Windows.Forms.View.Details;
+            lv_Books.FullRowSelect = true;
+            lv_Books.GridLines = true;
+            lv_Books.Columns.Add("", 0, HorizontalAlignment.Right);
+            lv_Books.Columns.Add("ISBN", 180, HorizontalAlignment.Left);
+            lv_Books.Columns.Add("Title Book", 210, HorizontalAlignment.Left);
+            lv_Books.Columns.Add("Writter Book", 110, HorizontalAlignment.Left);
+            lv_Books.Columns.Add("Genre Book", 100, HorizontalAlignment.Left);
+            lv_Books.Columns.Add("Publisher", 110, HorizontalAlignment.Left);
+            lv_Books.Columns.Add("Stocks", 100, HorizontalAlignment.Left);
         }
     }
 }
