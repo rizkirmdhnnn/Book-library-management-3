772d908ceae45d4fc7131447122e820b0595834b
rizpedia <achmadrizkiramadhan0101@gmail.com>
2023-12-03 10:14:13 +0700

    feat : Added Members view

--- Book-library-management-3/Views/MembersUC.Designer.cs
+++ Book-library-management-3/Views/MembersUC.Designer.cs
@@ -28,16 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MembersUC));
             this.lv_History = new System.Windows.Forms.ListView();
             this.guna2Panel9 = new Guna.UI2.WinForms.Guna2Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.btn_add = new Guna.UI2.WinForms.Guna2Button();
+            this.txt_total = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
             this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.btn_add = new Guna.UI2.WinForms.Guna2Button();
+            this.txtbox_username = new Guna.UI2.WinForms.Guna2TextBox();
             this.guna2Panel9.SuspendLayout();
+            this.guna2Panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lv_History
@@ -45,16 +46,15 @@
             this.lv_History.HideSelection = false;
             this.lv_History.Location = new System.Drawing.Point(28, 80);
             this.lv_History.Name = "lv_History";
-            this.lv_History.Size = new System.Drawing.Size(817, 445);
+            this.lv_History.Size = new System.Drawing.Size(817, 398);
             this.lv_History.TabIndex = 19;
             this.lv_History.UseCompatibleStateImageBehavior = false;
             // 
             // guna2Panel9
             // 
-            this.guna2Panel9.Controls.Add(this.label1);
-            this.guna2Panel9.Controls.Add(this.btn_add);
+            this.guna2Panel9.Controls.Add(this.guna2Panel1);
+            this.guna2Panel9.Controls.Add(this.txt_total);
             this.guna2Panel9.Controls.Add(this.label2);
-            this.guna2Panel9.Controls.Add(this.guna2TextBox1);
             this.guna2Panel9.Dock = System.Windows.Forms.DockStyle.Top;
             this.guna2Panel9.FillColor = System.Drawing.Color.White;
             this.guna2Panel9.Location = new System.Drawing.Point(0, 0);
@@ -64,38 +64,17 @@
             this.guna2Panel9.Size = new System.Drawing.Size(860, 63);
             this.guna2Panel9.TabIndex = 18;
             // 
-            // label1
+            // txt_total
             // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Montserrat", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(21, 6);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 44);
-            this.label1.TabIndex = 11;
-            this.label1.Text = "10";
-            // 
-            // btn_add
-            // 
-            this.btn_add.BackColor = System.Drawing.Color.Transparent;
-            this.btn_add.BorderRadius = 8;
-            this.btn_add.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.btn_add.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.btn_add.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.btn_add.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.btn_add.FillColor = System.Drawing.Color.Transparent;
-            this.btn_add.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_add.ForeColor = System.Drawing.Color.Black;
-            this.btn_add.Image = global::Book_library_management_3.Properties.Resources.Add_Option;
-            this.btn_add.ImageAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.btn_add.Location = new System.Drawing.Point(764, 15);
-            this.btn_add.Margin = new System.Windows.Forms.Padding(2);
-            this.btn_add.Name = "btn_add";
-            this.btn_add.Size = new System.Drawing.Size(81, 31);
-            this.btn_add.TabIndex = 10;
-            this.btn_add.Text = "Add";
-            this.btn_add.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txt_total.AutoSize = true;
+            this.txt_total.BackColor = System.Drawing.Color.Transparent;
+            this.txt_total.Font = new System.Drawing.Font("Montserrat", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_total.Location = new System.Drawing.Point(21, 6);
+            this.txt_total.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.txt_total.Name = "txt_total";
+            this.txt_total.Size = new System.Drawing.Size(54, 44);
+            this.txt_total.TabIndex = 11;
+            this.txt_total.Text = "10";
             // 
             // label2
             // 
@@ -109,28 +88,6 @@
             this.label2.TabIndex = 9;
             this.label2.Text = "Total Members";
             // 
-            // guna2TextBox1
-            // 
-            this.guna2TextBox1.BackColor = System.Drawing.Color.Transparent;
-            this.guna2TextBox1.BorderRadius = 8;
-            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.guna2TextBox1.DefaultText = "";
-            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
-            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
-            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
-            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.guna2TextBox1.Location = new System.Drawing.Point(546, 15);
-            this.guna2TextBox1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
-            this.guna2TextBox1.Name = "guna2TextBox1";
-            this.guna2TextBox1.PasswordChar = '\0';
-            this.guna2TextBox1.PlaceholderText = "Search";
-            this.guna2TextBox1.SelectedText = "";
-            this.guna2TextBox1.Size = new System.Drawing.Size(202, 32);
-            this.guna2TextBox1.TabIndex = 2;
-            // 
             // guna2Button1
             // 
             this.guna2Button1.BackColor = System.Drawing.Color.Transparent;
@@ -142,9 +99,9 @@
             this.guna2Button1.FillColor = System.Drawing.Color.Transparent;
             this.guna2Button1.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button1.ForeColor = System.Drawing.Color.Black;
-            this.guna2Button1.Image = global::Book_library_management_3.Properties.Resources.Edit;
+            this.guna2Button1.Image = global::Book_library_management_3.Properties.Resources.Add_Option;
             this.guna2Button1.ImageAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.guna2Button1.Location = new System.Drawing.Point(739, 530);
+            this.guna2Button1.Location = new System.Drawing.Point(739, 483);
             this.guna2Button1.Margin = new System.Windows.Forms.Padding(2);
             this.guna2Button1.Name = "guna2Button1";
             this.guna2Button1.Size = new System.Drawing.Size(106, 31);
@@ -163,15 +120,69 @@
             this.guna2Button2.FillColor = System.Drawing.Color.Transparent;
             this.guna2Button2.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button2.ForeColor = System.Drawing.Color.Black;
-            this.guna2Button2.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button2.Image")));
+            this.guna2Button2.Image = global::Book_library_management_3.Properties.Resources.Add_Option;
             this.guna2Button2.ImageAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.guna2Button2.Location = new System.Drawing.Point(613, 530);
+            this.guna2Button2.Location = new System.Drawing.Point(613, 483);
             this.guna2Button2.Margin = new System.Windows.Forms.Padding(2);
             this.guna2Button2.Name = "guna2Button2";
             this.guna2Button2.Size = new System.Drawing.Size(122, 31);
             this.guna2Button2.TabIndex = 21;
             this.guna2Button2.Text = "Delete Data";
             this.guna2Button2.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Panel1.Controls.Add(this.btn_add);
+            this.guna2Panel1.Controls.Add(this.txtbox_username);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.guna2Panel1.Location = new System.Drawing.Point(491, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(369, 63);
+            this.guna2Panel1.TabIndex = 13;
+            // 
+            // btn_add
+            // 
+            this.btn_add.BackColor = System.Drawing.Color.Transparent;
+            this.btn_add.BorderRadius = 8;
+            this.btn_add.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_add.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_add.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_add.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_add.FillColor = System.Drawing.Color.Transparent;
+            this.btn_add.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.ForeColor = System.Drawing.Color.Black;
+            this.btn_add.Image = global::Book_library_management_3.Properties.Resources.Add_Option;
+            this.btn_add.ImageAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.btn_add.Location = new System.Drawing.Point(273, 17);
+            this.btn_add.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(81, 31);
+            this.btn_add.TabIndex = 10;
+            this.btn_add.Text = "Add";
+            this.btn_add.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // txtbox_username
+            // 
+            this.txtbox_username.BackColor = System.Drawing.Color.Transparent;
+            this.txtbox_username.BorderRadius = 8;
+            this.txtbox_username.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtbox_username.DefaultText = "";
+            this.txtbox_username.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtbox_username.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtbox_username.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtbox_username.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtbox_username.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtbox_username.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtbox_username.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtbox_username.Location = new System.Drawing.Point(43, 17);
+            this.txtbox_username.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.txtbox_username.Name = "txtbox_username";
+            this.txtbox_username.PasswordChar = '\0';
+            this.txtbox_username.PlaceholderText = "Search";
+            this.txtbox_username.SelectedText = "";
+            this.txtbox_username.Size = new System.Drawing.Size(202, 32);
+            this.txtbox_username.TabIndex = 2;
             // 
             // MembersUC
             // 
@@ -184,10 +195,10 @@
             this.Font = new System.Drawing.Font("Montserrat", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.Name = "MembersUC";
-            this.Size = new System.Drawing.Size(860, 582);
-            this.Load += new System.EventHandler(this.MembersUC_Load);
+            this.Size = new System.Drawing.Size(860, 541);
             this.guna2Panel9.ResumeLayout(false);
             this.guna2Panel9.PerformLayout();
+            this.guna2Panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -196,11 +207,12 @@
 
         private System.Windows.Forms.ListView lv_History;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel9;
-        private System.Windows.Forms.Label label1;
-        private Guna.UI2.WinForms.Guna2Button btn_add;
+        private System.Windows.Forms.Label txt_total;
         private System.Windows.Forms.Label label2;
-        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
         private Guna.UI2.WinForms.Guna2Button guna2Button1;
         private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2Button btn_add;
+        private Guna.UI2.WinForms.Guna2TextBox txtbox_username;
     }
 }
--- Book-library-management-3/Views/MembersUC.cs
+++ Book-library-management-3/Views/MembersUC.cs
@@ -7,29 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Book_library_management_3.Models.Entity;
+using Book_library_management_3.Controllers;
 
 namespace Book_library_management_3.Views
 {
     public partial class MembersUC : UserControl
     {
+        private UsersControler _usersControler;
         public MembersUC()
         {
+            _usersControler = new UsersControler();
             InitializeComponent();
+            InitializeListView();
+            loadData();
         }
 
-        private void guna2TextBox1_TextChanged(object sender, EventArgs e)
-        {
 
+        private void loadData()
+        {
+            txt_total.Text  = _usersControler.getTotalMembers().ToString();
         }
-
-        private void MembersUC_Load(object sender, EventArgs e)
+        private void InitializeListView()
         {
-
-        }
-
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
+            lv_History.View = System.Windows.Forms.View.Details;
+            lv_History.FullRowSelect = true;
+            lv_History.GridLines = true;
+            lv_History.Columns.Add("", 0, HorizontalAlignment.Right);
+            lv_History.Columns.Add("Username", 180, HorizontalAlignment.Left);
+            lv_History.Columns.Add("Password",  110, HorizontalAlignment.Left);
+            lv_History.Columns.Add("Name", 210, HorizontalAlignment.Left);
+            lv_History.Columns.Add("Email", 100, HorizontalAlignment.Left);
+            lv_History.Columns.Add("Status", 110, HorizontalAlignment.Left);
+            lv_History.Columns.Add("Date Register", 100, HorizontalAlignment.Left);
         }
     }
 }
