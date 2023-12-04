d75282d2d16789089382b009abe639d46d2996f9
rizpedia <achmadrizkiramadhan0101@gmail.com>
2023-12-04 22:31:06 +0700

    style : styling button

--- Book-library-management-3/Views/Dashboard/Books_Section/BooksUC.Designer.cs
+++ Book-library-management-3/Views/Dashboard/Books_Section/BooksUC.Designer.cs
@@ -30,13 +30,13 @@
         {
             this.guna2Panel9 = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
-            this.btn_add = new Guna.UI2.WinForms.Guna2Button();
             this.txt_username = new Guna.UI2.WinForms.Guna2TextBox();
             this.txt_total = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.lv_Books = new System.Windows.Forms.ListView();
+            this.btn_add = new Guna.UI2.WinForms.Guna2Button();
             this.btn_Delete = new Guna.UI2.WinForms.Guna2Button();
             this.btn_Edit = new Guna.UI2.WinForms.Guna2Button();
-            this.lv_Books = new System.Windows.Forms.ListView();
             this.guna2Panel9.SuspendLayout();
             this.guna2Panel1.SuspendLayout();
             this.SuspendLayout();
@@ -58,35 +58,12 @@
             // guna2Panel1
             // 
             this.guna2Panel1.BackColor = System.Drawing.Color.Transparent;
-            this.guna2Panel1.Controls.Add(this.btn_add);
             this.guna2Panel1.Controls.Add(this.txt_username);
             this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Right;
             this.guna2Panel1.Location = new System.Drawing.Point(491, 0);
             this.guna2Panel1.Name = "guna2Panel1";
             this.guna2Panel1.Size = new System.Drawing.Size(369, 63);
             this.guna2Panel1.TabIndex = 12;
-            // 
-            // btn_add
-            // 
-            this.btn_add.BackColor = System.Drawing.Color.Transparent;
-            this.btn_add.BorderRadius = 8;
-            this.btn_add.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.btn_add.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.btn_add.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.btn_add.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.btn_add.FillColor = System.Drawing.Color.Transparent;
-            this.btn_add.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_add.ForeColor = System.Drawing.Color.Black;
-            this.btn_add.Image = global::Book_library_management_3.Properties.Resources.Add_Option;
-            this.btn_add.ImageAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.btn_add.Location = new System.Drawing.Point(273, 17);
-            this.btn_add.Margin = new System.Windows.Forms.Padding(2);
-            this.btn_add.Name = "btn_add";
-            this.btn_add.Size = new System.Drawing.Size(81, 31);
-            this.btn_add.TabIndex = 10;
-            this.btn_add.Text = "Add";
-            this.btn_add.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
             // 
             // txt_username
             // 
@@ -101,7 +78,7 @@
             this.txt_username.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
             this.txt_username.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.txt_username.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
-            this.txt_username.Location = new System.Drawing.Point(43, 17);
+            this.txt_username.Location = new System.Drawing.Point(132, 19);
             this.txt_username.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.txt_username.Name = "txt_username";
             this.txt_username.PasswordChar = '\0';
@@ -135,50 +112,6 @@
             this.label2.TabIndex = 9;
             this.label2.Text = "Total Books";
             // 
-            // btn_Delete
-            // 
-            this.btn_Delete.BackColor = System.Drawing.Color.Transparent;
-            this.btn_Delete.BorderRadius = 8;
-            this.btn_Delete.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.btn_Delete.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.btn_Delete.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.btn_Delete.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.btn_Delete.FillColor = System.Drawing.Color.Transparent;
-            this.btn_Delete.Font = new System.Drawing.Font("Montserrat", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_Delete.ForeColor = System.Drawing.Color.Black;
-            this.btn_Delete.Image = global::Book_library_management_3.Properties.Resources.Delete;
-            this.btn_Delete.ImageAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.btn_Delete.Location = new System.Drawing.Point(593, 535);
-            this.btn_Delete.Margin = new System.Windows.Forms.Padding(2);
-            this.btn_Delete.Name = "btn_Delete";
-            this.btn_Delete.Size = new System.Drawing.Size(122, 31);
-            this.btn_Delete.TabIndex = 23;
-            this.btn_Delete.Text = "Delete Data";
-            this.btn_Delete.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
-            // 
-            // btn_Edit
-            // 
-            this.btn_Edit.BackColor = System.Drawing.Color.Transparent;
-            this.btn_Edit.BorderRadius = 8;
-            this.btn_Edit.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.btn_Edit.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.btn_Edit.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.btn_Edit.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.btn_Edit.FillColor = System.Drawing.Color.Transparent;
-            this.btn_Edit.Font = new System.Drawing.Font("Montserrat", 9F);
-            this.btn_Edit.ForeColor = System.Drawing.Color.Black;
-            this.btn_Edit.Image = global::Book_library_management_3.Properties.Resources.Edit;
-            this.btn_Edit.ImageAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.btn_Edit.Location = new System.Drawing.Point(719, 535);
-            this.btn_Edit.Margin = new System.Windows.Forms.Padding(2);
-            this.btn_Edit.Name = "btn_Edit";
-            this.btn_Edit.Size = new System.Drawing.Size(106, 31);
-            this.btn_Edit.TabIndex = 22;
-            this.btn_Edit.Text = "Edit Data";
-            this.btn_Edit.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
-            this.btn_Edit.Click += new System.EventHandler(this.btn_Edit_Click);
-            // 
             // lv_Books
             // 
             this.lv_Books.Font = new System.Drawing.Font("Montserrat", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -189,13 +122,80 @@
             this.lv_Books.TabIndex = 24;
             this.lv_Books.UseCompatibleStateImageBehavior = false;
             // 
+            // btn_add
+            // 
+            this.btn_add.BackColor = System.Drawing.Color.Transparent;
+            this.btn_add.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(218)))), ((int)(((byte)(223)))));
+            this.btn_add.BorderRadius = 8;
+            this.btn_add.BorderThickness = 1;
+            this.btn_add.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_add.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_add.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_add.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_add.FillColor = System.Drawing.Color.Transparent;
+            this.btn_add.Font = new System.Drawing.Font("Montserrat SemiBold", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_add.Location = new System.Drawing.Point(29, 534);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.PressedColor = System.Drawing.Color.Transparent;
+            this.btn_add.Size = new System.Drawing.Size(126, 32);
+            this.btn_add.TabIndex = 3;
+            this.btn_add.Text = "Add New Book";
+            this.btn_add.TextFormatNoPrefix = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Delete.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(218)))), ((int)(((byte)(223)))));
+            this.btn_Delete.BorderRadius = 8;
+            this.btn_Delete.BorderThickness = 1;
+            this.btn_Delete.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_Delete.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_Delete.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_Delete.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_Delete.FillColor = System.Drawing.Color.Transparent;
+            this.btn_Delete.Font = new System.Drawing.Font("Montserrat SemiBold", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Delete.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Delete.Location = new System.Drawing.Point(634, 534);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.PressedColor = System.Drawing.Color.Transparent;
+            this.btn_Delete.Size = new System.Drawing.Size(100, 32);
+            this.btn_Delete.TabIndex = 25;
+            this.btn_Delete.Text = "Delete Data";
+            this.btn_Delete.TextFormatNoPrefix = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_Edit
+            // 
+            this.btn_Edit.BackColor = System.Drawing.Color.Transparent;
+            this.btn_Edit.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(213)))), ((int)(((byte)(218)))), ((int)(((byte)(223)))));
+            this.btn_Edit.BorderRadius = 8;
+            this.btn_Edit.BorderThickness = 1;
+            this.btn_Edit.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_Edit.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_Edit.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_Edit.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_Edit.FillColor = System.Drawing.Color.Transparent;
+            this.btn_Edit.Font = new System.Drawing.Font("Montserrat SemiBold", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Edit.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_Edit.Location = new System.Drawing.Point(740, 533);
+            this.btn_Edit.Name = "btn_Edit";
+            this.btn_Edit.PressedColor = System.Drawing.Color.Transparent;
+            this.btn_Edit.Size = new System.Drawing.Size(85, 32);
+            this.btn_Edit.TabIndex = 26;
+            this.btn_Edit.Text = "Edit Data";
+            this.btn_Edit.TextFormatNoPrefix = true;
+            this.btn_Edit.Click += new System.EventHandler(this.btn_Edit_Click);
+            // 
             // BooksUC
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.lv_Books);
-            this.Controls.Add(this.btn_Delete);
             this.Controls.Add(this.btn_Edit);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.lv_Books);
             this.Controls.Add(this.guna2Panel9);
             this.Name = "BooksUC";
             this.Size = new System.Drawing.Size(860, 582);
@@ -209,12 +209,12 @@
         #endregion
         private Guna.UI2.WinForms.Guna2Panel guna2Panel9;
         private Guna.UI2.WinForms.Guna2TextBox txt_username;
-        private Guna.UI2.WinForms.Guna2Button btn_add;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label txt_total;
-        private Guna.UI2.WinForms.Guna2Button btn_Delete;
-        private Guna.UI2.WinForms.Guna2Button btn_Edit;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
         private System.Windows.Forms.ListView lv_Books;
+        private Guna.UI2.WinForms.Guna2Button btn_add;
+        private Guna.UI2.WinForms.Guna2Button btn_Delete;
+        private Guna.UI2.WinForms.Guna2Button btn_Edit;
     }
 }
